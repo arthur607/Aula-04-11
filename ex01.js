bcbd05aa62c55b72e0bb25397b264a9aa57f4947
Your Namea <you@example.com>
2020-11-04 17:30:30 -0300

    segundo commit

---
+++ ex01.js
@@ -0,0 +1,22 @@
+/*comentário de bloco*/
+
+
+
+document.querySelector('#botao').addEventListener
+('click',() => {
+    let nome;
+    
+    nome = document.getElementById('nome').value;
+    let sobrenome=document.getElementById('sobrenome').value;
+    document.write(' O nome digitado é: ' +nome+' '+sobrenome)
+})
+
+document.querySelector('#botao').addEventListener(click,
+() => {
+    let nome = document.getElementById('nome').value;
+    let sobrenome = document.getElementById('sobrenome').value;
+
+   
+
+})
+alert('o nome digitado é '+ nome+''+sobrenome)
